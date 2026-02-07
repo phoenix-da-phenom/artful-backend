8f1dfca7d8510b9b48e5ad358677858a286e1a5c
phoenix-da-phenom <ehinormichael@ymail.com>
2026-02-07 20:34:32 +0100

    MongoDB implementqtion sucessful

--- server.js
+++ server.js
@@ -1,19 +1,20 @@
 require("dotenv").config();
-const express = require('express');
+const express = require("express");
 const cors = require("cors");
 const path = require("path");
-const fs = require();
+const connectDB = require("./src/config/db");
+const fs = require("fs"); // optional – only if you need it
 
+const PORT = 8000;
 
-const Port = 8000
+const app = express();
 
-const app = express()
+app.use(cors());
+app.use(express.json());
 
-app.use(express.json())
+// Connect to the database
+connectDB();
 
-
-
-
-app.listen(Port, () => {
-    console.log(`Server is running on ${Port}`)
-})
+app.listen(PORT, () => {
+    console.log(`Server is running on ${PORT}`);
+});
